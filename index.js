5ba760fd2d3ce45c7f1476a192618c5f40942c78
liambloom <liamrbloom@gmail.com>
2024-12-25 01:00:09 -0500

    Added flags and winning

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const numFont = new FontFace("VT323", `url(https://fonts.gstatic.com/s/vt323/v17/pxiKyp0ihIEF2isfFJU.woff2)`)
+numFont.load()
+    .then(font => {
+        document.fonts.add(font);
+    })
+    .catch(err => console.error(err));
+
 const c = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 
@@ -8,49 +15,46 @@ const bevelSize = 3;
 const bgColor = "#bbbbbb";
 const gridColor = "#888888";
 const numColors = ["blue", "green", "red", "darkblue", "brown", "cyan", "black", "#404040"];
+const canvasWidth = gameWidth * squareSize + 1;
+const canvasHeight = gameHeight * squareSize + 1;
+
+function arrayOf(length, gen) {
+    return Array.from({length}, gen);
+}
 
 const mine = Symbol("mine");
 const minecount = 30;
-const board = Array.from({length: gameWidth}, () => Array(gameHeight).fill(undefined));
+let remainingSafe = (gameWidth * gameHeight) - minecount;
+const board = arrayOf(gameWidth, () => arrayOf(gameHeight, () => ({
+    isVisible: false,
+    isMine: false,
+    hasFlag: false,
+    adjMines: 0,
+})))
 let mines = 0;
 while (mines < minecount) {
     const x = Math.floor(Math.random() * gameWidth);
     const y = Math.floor(Math.random() * gameHeight);
 
-    if (!board[x][y]) {
-        board[x][y] = mine;
+    if (!board[x][y].isMine) {
+        board[x][y].isMine = true;
         mines++;
     }
 }
 function isMine(x, y) {
-    if (x < 0 || x >= gameWidth || y < 0 || y >= gameHeight) {
-        return false;
-    }
-    else if (board[x][y] === mine) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return !(x < 0 || x >= gameWidth || y < 0 || y >= gameHeight) && board[x][y].isMine;
 }
 for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-        if (board[i][j] === mine) {
-            continue;
+        if (!board[i][j].isMine) {
+            for (let [x, y] of [[i-1, j-1], [i-1, j], [i-1, j+1], [i, j-1], [i, j+1], [i+1, j-1], [i+1, j], [i+1, j+1]]) {
+                board[i][j].adjMines += isMine(x, y);
+            }
         }
-        let adj = 0;
-        for (let [x, y] of [[i-1, j-1], [i-1, j], [i-1, j+1], [i, j-1], [i, j+1], [i+1, j-1], [i+1, j], [i+1, j+1]]) {
-            adj += isMine(x, y);
-        }
-        board[i][j] = adj;
     }
 }
-const boardVisibility = Array.from({length: gameWidth}, () => Array(gameHeight).fill(false));
 
-c.width = gameWidth * squareSize + 1;
-c.height = gameHeight * squareSize + 1;
-
-function drawFrame() {
+function drawWholeBoard() {
     ctx.clearRect(0, 0, c.width, c.height);
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, c.width, c.height);
@@ -72,14 +76,13 @@ function drawFrame() {
             ctx.save();
             ctx.translate(squareSize * x + 1, squareSize * y + 1);
 
-            if (boardVisibility[x][y]) {
-                console.log(board[x][y]);
-                if (board[x][y] > 0) {
-                    ctx.font = "20px Arial";
+            if (board[x][y].isVisible) {
+                if (board[x][y].adjMines > 0) {
+                    ctx.font = "24px VT323";
                     ctx.textAlign = "center";
                     ctx.textBaseline = "middle";
-                    ctx.fillStyle = numColors[board[x][y]];
-                    ctx.fillText(board[x][y] + "", (squareSize - 1) / 2, (squareSize - 1) / 2);
+                    ctx.fillStyle = numColors[board[x][y].adjMines - 1];
+                    ctx.fillText(board[x][y].adjMines + "", (squareSize - 1) / 2, (squareSize - 1) / 2);
                 }
             }
             else {
@@ -107,6 +110,26 @@ function drawFrame() {
 
                 ctx.fillStyle = "#a0a0a0";
                 ctx.fillRect(bevelSize, bevelSize, squareSize - bevelSize * 2 - 1, squareSize - bevelSize * 2 - 1);
+
+                if (board[x][y].hasFlag) {
+                    ctx.translate(bevelSize, bevelSize);
+                    ctx.beginPath();
+                    // ctx.moveTo(4, 19);
+                    // ctx.lineTo(16, 19);
+                    ctx.ellipse(10, 19, 7, 4, 0, 0, Math.PI, true);
+                    ctx.rect(9, 10, 2, 5);
+                    ctx.fillStyle = "black";
+                    ctx.fill();
+
+                    ctx.beginPath();
+                    ctx.moveTo(11, 1);
+                    ctx.lineTo(3, 5.5);
+                    ctx.lineTo(11, 10);
+                    ctx.closePath();
+                    ctx.fillStyle = "red";
+                    ctx.fill();
+                    // ctx.moveTo()
+                }
             }
 
             ctx.restore();
@@ -114,7 +137,31 @@ function drawFrame() {
     }
 }
 
-drawFrame();
+let resizeListenerRemover;
+function setCanvasSize() {
+    if (resizeListenerRemover !== undefined) {
+        resizeListenerRemover();
+    }
+
+    // c.width = ;
+// c.height = gameHeight * squareSize + 1;
+
+    c.width = canvasWidth * devicePixelRatio;
+    c.height = canvasHeight * devicePixelRatio;
+
+    c.style.width = canvasWidth + "px";
+    c.style.height = canvasHeight + "px";
+
+    ctx.scale(devicePixelRatio, devicePixelRatio);
+
+    const media = matchMedia(`(resolution: ${devicePixelRatio}dppx)`);
+    media.addEventListener("change", this.setCanvasSize);
+    resizeListenerRemover = () => media.removeEventListener("change", this.setCanvasSize);
+
+    drawWholeBoard();
+}
+
+setCanvasSize();
 
 function getClickCord(event) {
     let x, y;
@@ -140,12 +187,15 @@ function getClickCord(event) {
 }
 
 function digAt([x, y]) {
-    if (board[x][y] === mine) {
+    if (board[x][y].isMine) {
         gameOver(x, y);
     }
-    else {
+    else if (!board[x][y].hasFlag) {
         recurseShow(x, y);
-        drawFrame();
+        drawWholeBoard();
+        if (remainingSafe === 0) {
+            gameWin();
+        }
     }
 }
 
@@ -153,9 +203,10 @@ function recurseShow(x, y) {
     if (x < 0 || x >= gameWidth || y < 0 || y >= gameHeight) {
         return;
     }
-    if (!boardVisibility[x][y]) {
-        boardVisibility[x][y] = true;
-        if (board[x][y] === 0) {
+    if (!board[x][y].isVisible) {
+        board[x][y].isVisible = true;
+        remainingSafe--;
+        if (board[x][y].adjMines === 0) {
             recurseShow(x - 1, y - 1);
             recurseShow(x - 1, y);
             recurseShow(x - 1, y + 1);
@@ -169,16 +220,25 @@ function recurseShow(x, y) {
 }
 
 function flagAt([x, y]) {
-
+    if (!board[x][y].isVisible) {
+        board[x][y].hasFlag ^= true;
+        drawWholeBoard();
+    }
 }
 
 function gameOver(x, y) {
     console.log("Game Over");
 }
 
-c.addEventListener("click", event => {
+function gameWin() {
+    console.log("Game win");
+}
+
+function clickHandler(event) {
+    event.preventDefault();
     const cord = getClickCord(event);
-    if (cord && !boardVisibility[cord[0]][cord[1]]) {
+    if (cord && !board[cord[0]][cord[1]].isVisible) {
+        console.log(event.button);
         if (event.button === 0) {
             digAt(cord);
         }
@@ -186,7 +246,7 @@ c.addEventListener("click", event => {
             flagAt(cord);
         }
     }
-});
-c.addEventListener("contextmenu", event => {
-    event.preventDefault();
-})
+}
+
+c.addEventListener("click", clickHandler);
+c.addEventListener("contextmenu", clickHandler)
